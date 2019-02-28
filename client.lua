4a3fda4a228071acfa209a6d4395d390f180c728
AnonimowyXD <46579570+AnonimowyXD@users.noreply.github.com>
2019-02-28 16:18:30 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,20 +1,18 @@
 Citizen.CreateThread(function()
     local dict = "anim@mp_player_intmenu@key_fob@"
     
-	RequestAnimDict(dict)
-	while not HasAnimDictLoaded(dict) do
-		Citizen.Wait(0)
-	end
-    local handsup = false
-	while true do
-		Citizen.Wait(0)
-		if IsControlJustPressed(1, 303) then ---Klawisz U
-            if not handsup then
-                TaskPlayAnim(GetPlayerPed(-1), dict, "base", 8.0, 8.0, -1, 50, 0, false, false, false)
-                handsup = true
-            else
-                handsup = false
-                ClearPedTasks(GetPlayerPed(-1))
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Citizen.Wait(100)
+    end
+    local lock = false
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 303) then ---Klawisz U 
+            if not lock then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
+                StopAnimTask = true
+
             end
         end
     end
